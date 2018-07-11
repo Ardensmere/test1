0878b473570ccd2623a6f5f31e44d00b2e1bb05f
Ardensmere <41117777+Ardensmere@users.noreply.github.com>
2018-07-11 06:48:36 -0700

    Add files via upload

---
+++ application/views/header.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title><?=$title?></title>
+	<link rel="stylesheet" href="resources/styles/bootstrap_theme.css" />
+</head>
+
+<body>
+	<nav class="navbar">
+		<div class="container">
+			<div class="navbar-header">
+				<p class="navbar-brand">Navigation</p>
+			</div>
+		</div>
+	</nav>
---
+++ application/views/pages/about.php
@@ -0,0 +1,4 @@
+<h1><?=$title?></h1>
+<h1>This site is for testing CodeIgniter</h1>
+	
+<p>So there.</p>
